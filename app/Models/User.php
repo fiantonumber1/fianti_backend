245b805117e4ed893031c1e5c1a66e49629cef3e
fiantonumber1 <87689030+fiantonumber1@users.noreply.github.com>
2025-02-21 19:30:17 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,8 @@ class User extends Authenticatable
         'email',
         'password',
         'rule',
-        'waphonenumber'
+        'waphonenumber',
+        'is_verified'
     ];
 
     /**
